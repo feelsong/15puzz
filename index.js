5877eac8ad1ed5f28d2354297d5e104ee4b01ca0
Philip Song <philjunsong@gmail.com>
2020-08-31 21:27:45 -0700

    fix bugs

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import { ShaderPass } from './three.js/examples/jsm/postprocessing/ShaderPass.js
 import { FXAAShader } from './three.js/examples/jsm/shaders/FXAAShader.js';
 
 const txtfile = require('./grid-sprite.jpg');
-const txtfile2 = require('./cat.jpg');
+const txtfile2 = require('./meeko.jpg');
 const txtfile3 = require('./tri_pattern.jpg');
 
 
@@ -54,6 +54,7 @@ var obj3d = new THREE.Object3D();
 var group = new THREE.Group();
 
 var pieces = [];
+let initialPos = [];
 
 var params = {
 	edgeStrength: 3.0,
@@ -66,12 +67,50 @@ var params = {
 
 var coord = [];
 
+function solved(arr) {
+  var a  = arr.map((x)=> {
+
+      //pos === data
+      return x.pos === x.data;
+  })
+
+
+
+  return !a.filter(x=> false).length;
+}
+
+function reset() {
+
+
+}
+
+function getRandomPos() {
+  let arr = shuffleArray([...Array(16).keys()]);
+  return shuffleArray(arr);
+}
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    return array;
+}
+
 
 
 init();
 animate();
 
 function init() {
+
+
+  initialPos = getRandomPos();
+  console.log('initial Pos', initialPos);
+
 
 	container = document.createElement( 'div' );
 	document.body.appendChild( container );
@@ -105,9 +144,7 @@ function init() {
 
 	var light = new THREE.DirectionalLight( 0xddffdd, 0.6 );
 	light.position.set( 1, 1, 1 );
-
 	light.castShadow = true;
-
 	light.shadow.mapSize.width = 1024;
 	light.shadow.mapSize.height = 1024;
 
@@ -121,22 +158,8 @@ function init() {
 	light.shadow.camera.far = 1000;
 
 	scene.add( light );
-
-
-
 	scene.add( group );
 
-	//group.add( obj3d );
-
-	//
-	// var floorMaterial = new THREE.MeshLambertMaterial( { side: THREE.DoubleSide } );
-	//
-	// var floorGeometry = new THREE.PlaneBufferGeometry( 12, 12 );
-	// var floorMesh = new THREE.Mesh( floorGeometry, floorMaterial );
-	// floorMesh.rotation.x -= Math.PI * 0.5;
-	// floorMesh.position.y -= 1.5;
-	// group.add( floorMesh );
-	// floorMesh.receiveShadow = true;
 
 	var material = new THREE.MeshPhongMaterial( { color: 0xffaaff } );
 
@@ -147,39 +170,70 @@ function init() {
 	for (let i = 0; i < 4; i ++ ) {
 		for (let j = 0; j < 4; j ++ ) {
 				coord.push({z: 10 * i + 1 * i, x: 10*j+1*j-25 });
-
-
 				if (!(i===3 && j===3)) {
-					console.log(i,j);
-
 					let pGeo = new THREE.PlaneBufferGeometry( 10, 10 );
 
 					let plane = new THREE.Mesh(
 							pGeo,
 							planeMaterial
 						);
-						plane.position.y = 0;
+
+            plane.position.y = 0;
+            /*
 						plane.position.z = 10 * i + 1 * i;
 						plane.position.x = 10 * j + 1 * j - 25;
-						plane.rotation.x = - Math.PI / 2;
+            */
+
+            plane.rotation.x = - Math.PI / 2;
 
 						plane.data = i*4+j;
-						plane.pos = i*4 + j;
+						// plane.pos = i*4 + j;
 						let uv = uvMap16[uvTo[i*4+j]];
 						var uvs = new Float32Array( uv);
 						pGeo.setAttribute( 'uv', new THREE.BufferAttribute( uvs, 2 ) );
 						pieces.push(plane);
-						console.log('piecs', pieces);
-						group.add(plane);
+						//console.log('piecs', pieces);
+						//group.add(plane);
 				}
 		}
 	}
 
 
+  // console.log('initial pos', initialPos);
+  // console.log('coord', coord);
+  // pieces.forEach((piece, i) => {
+  //   console.log('pieces.data', piece.data)
+  //   let currPos = initialPos[i];
+  //   console.log('currPos', currPos);
+  //
+  //   piece.position.x = coord[currPos].x;
+  //   piece.position.z = coord[currPos].z;
+  //   piece.pos = currPos;
+  //   group.add(piece);
+  //
+  //   if (currPos === 15) {
+  //
+  //     _blank = i;
+  //   }
+  //
+  // });
 
+  initialPos.forEach((el, i)=> {
+    if (el !== 15) {
+      let currPiece = pieces.filter(piece => piece.data === el)[0];
 
+      currPiece.pos = i;
+      currPiece.position.x = coord[i].x;
+      currPiece.position.z = coord[i].z;
+      group.add(currPiece);
+    } else {
+      _blank = i;
+    }
 
+  })
 
+  console.log('_blank', _blank);
+  console.log('pieces', pieces);
 
 
 	// postprocessing
@@ -219,6 +273,7 @@ function init() {
 	renderer.domElement.addEventListener("click", onclick, true);
 
 	function onclick() {
+    // console.log('selectedObjects', selectedObjects)
 		console.log('clicked!', selectedObjects ? selectedObjects[0].data : null);
 		console.log('_blank!', _blank);
 		// get some data
@@ -226,90 +281,83 @@ function init() {
 		let pos = selectedObjects[0].pos;
 
 			console.log('index', index);
+      	console.log('pos', pos);
 
-		if (pos === (_blank - 1 ) ) {
-			 //left
-			 pieces[index].position.set(coord[_blank].x, 0, coord[_blank].z);
-			 pieces[index]['pos'] = _blank;
-			 _blank = pos;
-		} else if (pos === (_blank + 1 ) ) {
-			 //right
-			 pieces[index].position.set(coord[_blank].x, 0, coord[_blank].z);
-			  pieces[index]['pos'] = _blank;
-			 _blank  = pos;
-		} else if (pos === _blank + 4 ) {
-			pieces[index].position.set(coord[_blank].x, 0, coord[_blank].z);
-			pieces[index]['pos'] = _blank;
-			_blank = pos;
-		} else if (pos === _blank - 4 ) {
-			pieces[index].position.set(coord[_blank].x, 0, coord[_blank].z);
-			pieces[index]['pos'] = _blank;
-			_blank = pos;
-		} else {
-			if (
-				( [0,1,2,3].includes(pos) && [0,1,2,3].includes(_blank) )
-				||
-				( [4,5,6,7].includes(pos) && [4,5,6,7].includes(_blank) )
-				||
-				([8,9,10,11].includes(pos) && [8,9,10,11].includes(_blank) )
-				||
-				( [12,13,14,15].includes(pos) && [12,13,14,15].includes(_blank) )
-			) {
-				if (pos < _blank) {
-					let diff = _blank - pos;
-					for (let i = 0; i < diff; i ++) {
-						let curr = _blank - 1 - i;
-						let currPiece = pieces.filter(piece => piece.pos === curr)[0];
-						let currIndex = currPiece.data;
+		if (
+			( [0,1,2,3].includes(pos) && [0,1,2,3].includes(_blank) )
+			||
+			( [4,5,6,7].includes(pos) && [4,5,6,7].includes(_blank) )
+			||
+			([8,9,10,11].includes(pos) && [8,9,10,11].includes(_blank) )
+			||
+			( [12,13,14,15].includes(pos) && [12,13,14,15].includes(_blank) )
+		) {
 
-						pieces[currIndex].position.set(coord[_blank-i].x, 0, coord[_blank-i].z);
-						pieces[currIndex]['pos'] = _blank -i;
-					}
-				} else {
-					let diff = pos - _blank;
-					for (let i = 0; i < diff; i ++) {
-						let curr = _blank + 1 + i;
-						let currPiece = pieces.filter(piece => piece.pos === curr)[0];
-						let currIndex = currPiece.data;
-						pieces[currIndex].position.set(coord[_blank+i].x, 0, coord[_blank+i].z);
-						pieces[currIndex]['pos'] = _blank + i;
-					}
+        console.log('same row');
+			if (pos < _blank) {
+          console.log('left');
 
+				let diff = _blank - pos;
+				for (let i = 0; i < diff; i ++) {
+					let curr = _blank - 1 - i;
+					let currPiece = pieces.filter(piece => piece.pos === curr)[0];
+					let currIndex = currPiece.data;
+
+					pieces[currIndex].position.set(coord[_blank-i].x, 0, coord[_blank-i].z);
+					pieces[currIndex]['pos'] = _blank -i;
 				}
-				_blank = pos;
-			} else if ((pos - _blank) % 4 === 0){
-				if (pos < _blank) {
-					let diff = parseInt((_blank - pos)/4);
-					for (let i = 0; i < diff; i ++) {
-						let curr = _blank - ((i+1)*4);
-						let currPiece = pieces.filter(piece => piece.pos === curr)[0];
-						let currIndex = currPiece.data;
-						pieces[currIndex].position.set(coord[_blank-i*4].x, 0, coord[_blank-i*4].z);
-						pieces[currIndex]['pos'] = _blank - i*4;
-					}
-
-
-				} else {
-					let diff = parseInt((pos - _blank)/4);
-					for (let i = 0; i < diff; i ++) {
-						let curr = _blank + ((i+1)*4);
-						let currPiece = pieces.filter(piece => piece.pos === curr)[0];
-						let currIndex = currPiece.data;
-						pieces[currIndex].position.set(coord[_blank+i*4].x, 0, coord[_blank+i*4].z);
-						pieces[currIndex]['pos'] = _blank  + i*4;
-					}
-
-
-				}
-				_blank = pos;
-
 			} else {
-				// not a vaild move
-				console.log('no move');
+            console.log('right');
+				let diff = pos - _blank;
+				for (let i = 0; i < diff; i ++) {
+					let curr = _blank + 1 + i;
+					let currPiece = pieces.filter(piece => piece.pos === curr)[0];
+					let currIndex = currPiece.data;
+					pieces[currIndex].position.set(coord[_blank+i].x, 0, coord[_blank+i].z);
+					pieces[currIndex]['pos'] = _blank + i;
+				}
+
 			}
 
+      _blank = pos;
 
+
+		} else if ((pos - _blank) % 4 === 0){
+      console.log('same column');
+
+			if (pos < _blank) {
+        console.log('bottom');
+
+				let diff = parseInt((_blank - pos)/4);
+				for (let i = 0; i < diff; i ++) {
+					let curr = _blank - ((i+1)*4);
+					let currPiece = pieces.filter(piece => piece.pos === curr)[0];
+					let currIndex = currPiece.data;
+					pieces[currIndex].position.set(coord[_blank-i*4].x, 0, coord[_blank-i*4].z);
+					pieces[currIndex]['pos'] = _blank - i*4;
+				}
+			} else {
+        console.log('top');
+
+				let diff = parseInt((pos - _blank)/4);
+				for (let i = 0; i < diff; i ++) {
+					let curr = _blank + ((i+1)*4);
+					let currPiece = pieces.filter(piece => piece.pos === curr)[0];
+					let currIndex = currPiece.data;
+					pieces[currIndex].position.set(coord[_blank+i*4].x, 0, coord[_blank+i*4].z);
+					pieces[currIndex]['pos'] = _blank  + i*4;
+				}
+
+			}
+			_blank = pos;
+		} else {
+
+
+
+			// not a vaild move
+			console.log('no move');
 		}
+
 }
 
 
